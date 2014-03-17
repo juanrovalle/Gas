9a7f05b6afdadb9a37c90588072ab42bfecefb03
jrovalles <jrovalles@DTOD-DEV-99.orange.com.do>
2014-03-17 11:26:48 -0400

    Signed-off-by: jrovalles <jrovalles@DTOD-DEV-99.orange.com.do>

--- Sales/src/Two.java
+++ Sales/src/Two.java
@@ -1,5 +1,5 @@
 public class Two {
 	public static void main(String[] args) {
-		System.out.println(args == null ? new String[] { "hello" } : args);
+		System.out.println(args == null ? new String[] { "helo" } : args);
 	}
 }
